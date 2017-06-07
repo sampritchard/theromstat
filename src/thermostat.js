4daeb67250bd3ace1c26a5691445c34d81efea2e
Sam Pritchard <sampritchard@msn.com>
2017-06-07 16:47:36 +0100

    isMAximumTemp changed and created MAX_TEMP constant

--- src/thermostat.js
+++ src/thermostat.js
@@ -6,17 +6,15 @@ function Thermostat() {
   this.powerSavingMode = true;
   this.MAXIMUM_TEMPERATURE_SAVING_MODE_ON = 25;
   this.MAXIMUM_TEMPERATURE_SAVING_MODE_OFF = 32;
-}
+  this.MAX_TEMP = this.MAXIMUM_TEMPERATURE_SAVING_MODE_ON;
+};
 
 Thermostat.prototype.up = function(){
   // this.temperature += 1;
   // console.log(this.temperature);
-  if(this.temperature >= 25){
-    console.log(1);
-    console.log(this.powerSavingMode);
-    console.log(2);
-    return this.isMaximumTemperature;
-    console.log(this.temperature);
+  this.isMaximumTemperature();
+  if(this.temperature >= this.MAX_TEMP){
+    return 'TEMP TOO HIGH';
   }else{
     return this.temperature += 1;
   };
@@ -24,35 +22,20 @@ Thermostat.prototype.up = function(){
 
 Thermostat.prototype.down = function(){
   if (this.temperature <= this.MINIMUM_TEMPERATURE){
-    this.temperature = 10;
+    return this.temperature = 10;
   }else{
-  this.temperature -= 1;
+  return this.temperature -= 1;
+  }
 };
 
-// Thermostat.prototype.powerSavingModeOn = function(){
-//   return this.powerSavingMode = true;
-// };
-//
-// Thermostat.prototype.powerSavingModeOff = function(){
-//   return this.powerSavingMode = false;
-// };
-
-
 Thermostat.prototype.switchPowerSavingMode = function(){
-  // return this.powerSavingMode = !this.powerSavingMode;
-  // if (this,powerSavingMode === true){
-  //   return this.powerSavingMode = false;
-  // } else{
-  //   return this.powerSavingMode = true;
-  // };
-  return true
+  return this.powerSavingMode = !this.powerSavingMode;
 };
 
 Thermostat.prototype.isMaximumTemperature = function() {
   if(this.powerSavingMode === false) {
-    return this.temperature = this.MAXIMUM_TEMPERATURE_SAVING_MODE_OFF;
+    return this.MAX_TEMP = this.MAXIMUM_TEMPERATURE_SAVING_MODE_OFF;
+  } else {
+    return this.MAX_TEMP = this.MAXIMUM_TEMPERATURE_SAVING_MODE_ON;
   }
-  return this.temperature = this.MAXIMUM_TEMPERATURE_SAVING_MODE_ON;
-};
-
 };
