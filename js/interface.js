010ad22f9c2e7490703fd6914837f672f1023b92
Lubos Michalic <lnmichalic@gmail.com>
2017-06-08 11:52:41 +0100

    API read current weather

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 
   thermostat = new Thermostat();
@@ -34,4 +36,21 @@ $(document).ready(function() {
   seeTemperature();
  })
 
+
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+}
+
+// displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
 })
